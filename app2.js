8212083a106d883aad3b930086d66d829a0da89d
Jay Walker <jwalker.nationwide@gmail.com>
2017-06-23 01:49:18 -0400

    updates using ES6

--- app2.js
+++ app2.js
@@ -1,20 +1,36 @@
 var util = require('util');
+'use strict';
 
-function Person() {
-    this.firstname = 'John';
-    this.lastname = 'Doe';
+//es 6 Class creation
+
+class Person { //Simplified way to write a constructor(Class Creator)
+    constructor(firstname, lastname) { //Constructor  params only used when object is created "new" keyword used
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        //everthing in this area will be added to the prototype chain
+    greet() {
+        console.log('Hello ' + this.firstname + ' ' + this.lastname); //replaces prototype
+    }
 }
-Person.prototype.greet = function() {
-    console.log('Hello ' + this.firstname + ' ' + this.lastname);
-};
 
-function Policeman() {
-    Person.call(this); // this line was added so the object can inherit Person since it was never called
-    this.badgenumber = 1234;
+
+
+
+class Policeman extends Person { //use extends to point to the parent class
+    constructor(firstname, lastname) {
+        super(firstname, lastname); // inherit from the parent class replacing "Person.call(this) in function constructors"
+        this.badgenumber = 1234;
+    }
 }
 
-util.inherits(Policeman, Person);
 
-var officer = new Policeman(); //called only the Policeman function "Person.Call"  was used to in the Policeman function constructror to get props and methods from Person
+
+// var john = new Person();
+// john.greet();
+
+//util.inherits(Policeman, Person); //connected the protypes - Replaced by "extends" key word
+
+var officer = new Policeman('Jay', 'Walker'); //called only the Policeman function "Person.Call"  was used to in the Policeman function constructror to get props and methods from Person
 
 officer.greet();
